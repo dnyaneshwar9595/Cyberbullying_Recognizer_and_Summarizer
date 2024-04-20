15e6f4c26c3d6a31e3febfa1ebadc7541982862a
dnyaneshwar9595 <dnyaneshwar8029@gmail.com>
2024-04-21 00:03:45 +0530

    created Flask app

--- app.py
+++ app.py
@@ -1,117 +1,48 @@
-import spacy
-import re
 import math
-import pandas as pd
-import numpy as np
-from spacy.lang.en.stop_words import STOP_WORDS
-from string import punctuation
-nlp = spacy.load("en_core_web_sm")
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from heapq import nlargest
-import string
-from definations import *
 import pickle
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from definations import *
+from heapq import nlargest
 
+# Loded Text Recognizer model
 with open('model.pkl', 'rb') as f:
     cv_loaded, clf_loaded = pickle.load(f)
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route("/submit", methods=["GET", "POST"])
+def process_text():
     if request.method == "POST":
-        text = request.form["text"]
-        if text:
+        operation = request.form['operation']
+        text = request.form['text']
+        cleaned_text = clean(text)
+        # print(cleaned_text)
+        if operation == 'recognize':
+            
             # Transform the input text using the loaded CountVectorizer
-            text_transformed = cv_loaded.transform([text]).toarray()
+            text_transformed = cv_loaded.transform([cleaned_text]).toarray()
             # Predict using the loaded classifier
             prediction = clf_loaded.predict(text_transformed)
-            result = "Hatred" if prediction == 0 else "Offensive" if prediction == 1 else "Neutral"
-            return render_template('index.html', prediction=result)
-    return render_template('index.html', prediction=None)
+            result = ''
+            if prediction[0][0]==1:
+                result= "Hatred speech"
+            elif prediction[0][1]==1:
+                result= "Offensive speech"
+            else:
+                result= "Neutral speech"
+        
+        else:
+            # Create an instance of Summarize with cleaned text
+            summarizer = Summarize(cleaned_text)
+            sent_score = summarizer.summarize_text()
+            num_lines = math.ceil(len(sent_score) * 0.3)
+            result = nlargest(n=num_lines, iterable=sent_score, key=sent_score.get)
+            result = [sent.text for sent in result]  # Extract text from Span objects
 
+        return jsonify({'operation': operation, 'result': result})
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-#************************************************************Offensiveness and sentiment analysis***********************************************************
-# Intializing stopwords and punctuation
-stopword =list(STOP_WORDS)
-punctuation = punctuation + '\n'
-
-# Read csv file from system and creating dataframe and labelling it.
-df = pd.read_csv("twitter_data.csv")
-df['labels'] = df['class'].map({0 : "Hate Speech Detected" , 1: "Offensive language Detected" , 2: "No hate and offesive speech" })
-df = df[['tweet', 'labels']]
-
-
-# applying cleaning to the text
-#Cleaning the data
-def clean(text):
-    text = str(text).lower()
-    text = re.sub('\[.*?\]','',text)
-    text = re.sub('https?://S+|www\.\S+','',text)
-    text = re.sub('[%s]'% re.escape(string.punctuation),'',text)
-    text = re.sub('\n','',text)
-    text = re.sub('\w*\d\w*','', text)
-    text = [word for word in text.split(' ') if word not in stopword]
-    text = " ".join(text)
-    return text
-
-df["tweet"] = df["tweet"].apply(clean)
-
-
-# creating x and y
-x = np.array(df["tweet"])
-y = np.array(df["labels"])
-
-
-# Fit and train the model using tweet data
-cv = CountVectorizer()
-x = cv.fit_transform(x)
-x_train,x_test,y_train,y_test = train_test_split(x , y , test_size=.33 , random_state= 42)
-clf = DecisionTreeClassifier()
-clf.fit(x_train,y_train)
-
-
-
-#***********************************************************************Text summarizer***************************************************************************
-doc = nlp(test_data)
-tokens = [token.text for token in doc]
-
-# Text cleaning providing word frequency counter
-word_freq = {}
-stop_words = list(STOP_WORDS)
-word_freq = word_freq_counter(doc , word_freq)
-
-#Normalizing the word counter 
-max_freq = max(word_freq.values())
-for word in word_freq.keys():
-    word_freq[word] = word_freq[word] / max_freq
-  
-# sentence tokenizer
-sent_token = [sent for sent in doc.sents]
-sent_score = {}
-sent_score = sentence_score(sent_token , sent_score , word_freq)
-
-
-
-#**************************************************************************Output************************************************************************************
-
-# selecting 30% sentence with max score and summarizing it
-if choice==1:
-    num_lines = math.ceil(len(sent_score) * .3)
-    summary = nlargest(n = num_lines, iterable = sent_score , key = sent_score.get)
-    print("\n\n Summary of the text is : \n\n")
-    print(summary)
-
-
-# Predicting the speech sentiments and offensiveness
-if choice==2:
-    df = cv.transform([test_data]).toarray()
-    print("\n\n sentiments/Offensiveness of the text is : \n\n")
-    print(clf.predict(df))
-            
--- definations.py
+++ definations.py
@@ -1,22 +1,31 @@
 import re
+import string
+import spacy
+import pandas as pd
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
-import string
+nlp = spacy.load("en_core_web_sm")
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+import pickle
 
+# Read csv file from system and creating dataframe and labelling it.
+df = pd.read_csv("twitter_data.csv")
+x = df["tweet"]
+y = df['class']
 
-#Introduction to menu
-def intro():
-    print("""\n\n**********************************************Choice Menu************************************************
-                                    
-        *************************************Text speech summurizer and its cyber Bullyeing detection*************************************
-                                        
-                                        1. Check the summary of the Text Input
-                                        2. Check whether the Lnaguge was Offensive or not
-                                        
-               \n\n """)
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(y)
+# Perform one-hot encoding
+onehot_encoder = OneHotEncoder()
+y_encoded = onehot_encoder.fit_transform(integer_encoded.reshape(-1, 1)).toarray()
 
-#Cleaning the data
-def clean(text , stopword):
+stopword =list(STOP_WORDS)
+punctuation = punctuation + '\n'
+# # #Cleaning the data
+def clean(text):
     text = str(text).lower()
     text = re.sub('\[.*?\]','',text)
     text = re.sub('https?://S+|www\.\S+','',text)
@@ -27,28 +36,60 @@ def clean(text , stopword):
     text = " ".join(text)
     return text
 
+# # applying cleaning to the text
+x_clean = x.apply(clean)
 
+# # Fit and train the model using tweet data
+cv = CountVectorizer()
+x_trans = cv.fit_transform(x_clean)
+x_train,x_test,y_train,y_test = train_test_split(x_trans , y_encoded , test_size=.33 , random_state= 42)
+clf = DecisionTreeClassifier()
+model = clf.fit(x_train,y_train)
 
-#word requency counter for summarizer
-def word_freq_counter(doc , word_freq):
-    stop_words = list(STOP_WORDS)
-    for word in doc:
-        if word.text.lower() not in stop_words:
-            if word.text.lower() not in punctuation:
+pickle.dump((cv, model), open('model.pkl', 'wb'))
+
+# Code for Summarizer
+
+class Summarize:
+    def __init__(self, test_data) -> None:
+        self.test_data = test_data
+        self.nlp = spacy.load('en_core_web_sm')
+        self.doc = self.nlp(self.test_data)
+        self.tokens = [token.text for token in self.doc]
+
+    @staticmethod
+    def word_freq_counter(doc):
+        word_freq = {}
+        stop_words = list(STOP_WORDS)
+        for word in doc:
+            if word.text.lower() not in stop_words and word.text.lower() not in punctuation:
                 if word.text not in word_freq.keys():
                     word_freq[word.text] = 1
                 else:
-                    word_freq[word.text] +=1
-    return word_freq
+                    word_freq[word.text] += 1
+        return word_freq
 
+    @staticmethod
+    def sentence_score(doc, word_freq):
+        sent_score = {}
+        sent_token = [sent for sent in doc.sents]
+        for sent in sent_token:
+            for word in sent:
+                if word.text.lower() in word_freq.keys():
+                    if sent not in sent_score.keys():
+                        sent_score[sent] = word_freq[word.text.lower()]
+                    else:
+                        sent_score[sent] += word_freq[word.text.lower()]  
+        return sent_score  
 
-# Calculating sentence score 
-def sentence_score(sent_token , sent_score , word_freq):
-    for sent in sent_token:
-        for word in sent:
-            if word.text.lower() in word_freq.keys():
-                if sent not in sent_score.keys():
-                    sent_score[sent] = word_freq[word.text.lower()]
-                else:
-                    sent_score[sent] += word_freq[word.text.lower()]  
-    return sent_score                  
+    def summarize_text(self):
+        doc = self.nlp(self.test_data)
+        word_freq = self.word_freq_counter(doc)
+        max_freq = max(word_freq.values())
+        for word in word_freq.keys():
+            word_freq[word] = word_freq[word] / max_freq
+        sent_score = self.sentence_score(doc, word_freq)
+        return sent_score
+
+ 
+
