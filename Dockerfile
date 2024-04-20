a52508163074b82f289be162fc14da503195570f
dnyaneshwar9595 <dnyaneshwar8029@gmail.com>
2024-04-21 03:42:37 +0530

    spacy added in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM python:3.9
 WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
+# Download spaCy model
+RUN python -m spacy download en_core_web_sm
 EXPOSE 5000
 CMD ["python", "app.py"]
 
--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def process_text():
 
         return jsonify({'operation': operation, 'result': result})
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port="8080")
